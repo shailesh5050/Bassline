24e0c2c1527ab84c7a5be533ef5c6350d2dd4a5f
shailesh5050 <shaileshkuswaha1@gmail.com>
2025-05-27 20:58:43 +0530

    Added : Health check route

--- user.admin/src/index.ts
+++ user.admin/src/index.ts
@@ -1,8 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+
 const app = express();
 const port = process.env.PORT || 8001;
+
+app.get('/', (req, res) => {
+    res.send('Hello from User Admin Service!');
+});
 app.listen(port, () => {
     console.log('Server is running on port '+ port);
 });
